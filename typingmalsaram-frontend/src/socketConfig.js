367d1cdd385de4a0233147efdec99c847398ac15
pranav madhu <pranavmadhu73@gmail.com>
2022-10-24 13:39:10 +0530

    added transports in socket client side

--- typingmalsaram-frontend/src/socketConfig.js
+++ typingmalsaram-frontend/src/socketConfig.js
@@ -1,4 +1,6 @@
-import io from 'socket.io-client'
+import io from "socket.io-client";
 // const socket = io('http://192.168.29.177:3001')
-const socket = io('http://localhost:3001')
+const socket = io("http://localhost:3001", {
+  transports: ["websocket", "polling", "flashsocket"],
+});
 export default socket;
